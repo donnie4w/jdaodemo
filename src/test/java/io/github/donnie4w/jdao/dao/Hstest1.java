96309e3cb1f872556a37de6ad783868a12f684f7
donnie4w <donnie4w@gmail.com>
2024-07-26 12:26:20 +0800

    Update Hstest1.java

--- src/test/java/io/github/donnie4w/jdao/dao/Hstest1.java
+++ src/test/java/io/github/donnie4w/jdao/dao/Hstest1.java
@@ -25,22 +25,21 @@ import io.github.donnie4w.jdao.handle.JdaoException;
 import io.github.donnie4w.jdao.util.Serializer;
 import java.util.Map;
 import java.util.HashMap;
-import java.io.Serializable;
 import java.util.Arrays;
 import java.util.Objects;
 
 
 /**
- * dbtype:mysql ,database:hstest ,table:hstest1
+ * dbtype:sqlite ,database:hstest.db ,table:hstest1
  *
  * @version jdao version 2.0.1
- * @date 2024-07-19 21:12:49 
+ * @date 2024-07-24 14:37:41 
  */
-public class Hstest1 extends Table<Hstest1> implements Serializable {
+public class Hstest1 extends Table<Hstest1> {
 
-    private static final long serialVersionUID = 6118074828633154000L;
+	private static final long serialVersionUID = 6118074828633154000L;
 
-    private final static String TABLENAME_ = "hstest1";
+	private final static String TABLENAME_ = "hstest1";
 
 	public final static Fields<Hstest1> ID = new Fields("id");
 	public final static Fields<Hstest1> ROWNAME = new Fields("rowname");
@@ -48,64 +47,67 @@ public class Hstest1 extends Table<Hstest1> implements Serializable {
 	public final static Fields<Hstest1> GOTO = new Fields("goto");
 	
 	public Hstest1() {
-        super(TABLENAME_, Hstest1.class);
-        super.initFields(ID,ROWNAME,VALUE,GOTO);
-    }
+		super(TABLENAME_, Hstest1.class);
+		super.initFields(ID,ROWNAME,VALUE,GOTO);
+	}
 
-    public Hstest1(String tableName) {
-        super(tableName, Hstest1.class);
-        super.initFields(ID,ROWNAME,VALUE,GOTO);
-    }
+	public Hstest1(String tableName) {
+		super(tableName, Hstest1.class);
+		super.initFields(ID,ROWNAME,VALUE,GOTO);
+	}
 
-    public void toJdao() {
-        super.init(Hstest1.class);
-    }
+	@Override
+	public void toJdao() {
+		super.init(Hstest1.class);
+	}
 
-	private int id;
+	private long id;
 	private String rowname;
 	private byte[] value;
 	private byte[] goto_;
 
-    public int getId() {
-        return this.id;
-    }
+	public long getId() {
+		return this.id;
+	}
 
-    public void setId(int id) {
-        fieldPut(ID, id);
-        this.id = id;
-    }
+	public void setId(long id) {
+		fieldPut(ID, id);
+		this.id = id;
+	}
 
-    public String getRowname() {
-        return this.rowname;
-    }
+	public String getRowname() {
+		return this.rowname;
+	}
 
-    public void setRowname(String rowname) {
-        fieldPut(ROWNAME, rowname);
-        this.rowname = rowname;
-    }
+	public void setRowname(String rowname) {
+		fieldPut(ROWNAME, rowname);
+		this.rowname = rowname;
+	}
 
-    public byte[] getValue() {
-        return this.value;
-    }
+	public byte[] getValue() {
+		return this.value;
+	}
 
-    public void setValue(byte[] value) {
-        fieldPut(VALUE, value);
-        this.value = value;
-    }
+	public void setValue(byte[] value) {
+		fieldPut(VALUE, value);
+		this.value = value;
+	}
 
-    public byte[] getGoto() {
-        return this.goto_;
-    }
+	public byte[] getGoto() {
+		return this.goto_;
+	}
 
-    public void setGoto(byte[] goto_) {
-        fieldPut(GOTO, goto_);
-        this.goto_ = goto_;
-    }
+	public void setGoto(byte[] goto_) {
+		fieldPut(GOTO, goto_);
+		this.goto_ = goto_;
+	}
 
-    public String toString() {
-        return "id:" + id + " , " + "rowname:" + rowname + " , " + "value:" + value + " , " + "goto:" + goto_;
-    }
+	@Override
+	public String toString() {
+		return "id:" + id + " , " + "rowname:" + rowname + " , " + "value:" + value + " , " + "goto:" + goto_;
+	}
 
+	@Override
 	public Hstest1 copy(Hstest1 h) {
 		this.setId(h.getId());
 		this.setRowname(h.getRowname());
@@ -114,27 +116,29 @@ public class Hstest1 extends Table<Hstest1> implements Serializable {
 		return this;
 	}
 
-    public void scan(String fieldname, Object obj) throws JdaoException {
-        try {
-            switch (fieldname) {
-                case "id":
-                    setId(Util.asInt(obj));
-                    break;
-                case "rowname":
-                    setRowname(Util.asString(obj));
-                    break;
-                case "value":
-                    setValue(Util.asBytes(obj));
-                    break;
-                case "goto":
-                    setGoto(Util.asBytes(obj));
-                    break;
-            }
-        } catch (Exception e) {
-             throw new JdaoException(e);
-        }
+	@Override
+	public void scan(String fieldname, Object obj) throws JdaoException {
+		try {
+			switch (fieldname) {
+				case "id":
+					setId(Util.asLong(obj));
+ 					break;
+				case "rowname":
+					setRowname(Util.asString(obj));
+ 					break;
+				case "value":
+					setValue(Util.asBytes(obj));
+ 					break;
+				case "goto":
+					setGoto(Util.asBytes(obj));
+ 					break;
+			}
+		} catch (Exception e) {
+			throw new JdaoException(e);
+		}
 	}
 
+	@Override
 	public byte[] encode() {
 		Map<String, Object> map = new HashMap();
 		map.put("id", this.getId());
@@ -144,6 +148,7 @@ public class Hstest1 extends Table<Hstest1> implements Serializable {
 		return Serializer.encode(map);
 	}
 
+	@Override
 	public Hstest1 decode(byte[] bs) throws JdaoException {
 		Map<String, Object> map = Serializer.decode(bs);
 		if (map != null) {
@@ -154,16 +159,16 @@ public class Hstest1 extends Table<Hstest1> implements Serializable {
 		return this;
 	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
         Hstest1 _Hstest1 = (Hstest1) o;
-        return id == _Hstest1.id && Objects.equals(rowname, _Hstest1.rowname) && Objects.deepEquals(value, _Hstest1.value) && Objects.deepEquals(goto_, _Hstest1.goto_);
-    }
+		return id == _Hstest1.id && Objects.equals(rowname, _Hstest1.rowname) && Objects.deepEquals(value, _Hstest1.value) && Objects.deepEquals(goto_, _Hstest1.goto_);
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id , rowname , Arrays.hashCode(value) , Arrays.hashCode(goto_));
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(id , rowname , Arrays.hashCode(value) , Arrays.hashCode(goto_));
+	}
 }
