615a4f91db2b646d4d597dee4c25144b2066bfeb
donnie4w <donnie4w@gmail.com>
2024-07-30 09:45:48 +0800

    Update DataTransfer.java

--- src/test/java/io/github/donnie4w/jdao/practice/DataTransfer.java
+++ src/test/java/io/github/donnie4w/jdao/practice/DataTransfer.java
@@ -3,10 +3,7 @@ package io.github.donnie4w.jdao.practice;
 import io.github.donnie4w.jdao.action.DataSourceFactory;
 import io.github.donnie4w.jdao.base.Table;
 import io.github.donnie4w.jdao.dao.Hstest1;
-import io.github.donnie4w.jdao.handle.DBType;
-import io.github.donnie4w.jdao.handle.Jdao;
-import io.github.donnie4w.jdao.handle.JdaoClassException;
-import io.github.donnie4w.jdao.handle.JdaoException;
+import io.github.donnie4w.jdao.handle.*;
 import org.junit.Test;
 
 import javax.sql.DataSource;
@@ -18,10 +15,8 @@ import java.util.List;
 public class DataTransfer {
 
 
-    public <T extends Table<?>> void saveByClass(Class<T> clz, List<T> list) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, JdaoException, SQLException {
-        DataSource dataSource = DataSourceFactory.getDataSourceByPostgre();
-        Table toTable = clz.getDeclaredConstructor().newInstance();
-        toTable.useDataSource(dataSource, DBType.POSTGRESQL);
+    public <T extends Table<?>> void saveByClass(DBhandle dBhandle, Class<T> clz, List<T> list) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, JdaoException, SQLException {
+        Table toTable = clz.getDeclaredConstructor().newInstance().useDBhandle(dBhandle);
         for (T t : list) {
             toTable.copy(t);
             toTable.addBatch();
@@ -30,18 +25,18 @@ public class DataTransfer {
     }
 
 
-    public <T extends Table<?>> void transferByClass(Class<T> clz) throws JdaoException, SQLException, JdaoClassException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+    public <T extends Table<?>> void transferByClass(DBhandle toDB, Class<T> clz) throws JdaoException, SQLException, JdaoClassException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         DataSource dataSource = DataSourceFactory.getDataSourceByMysql();
-        Jdao.setDataSource(clz, dataSource, DBType.MYSQL);
+        Jdao.bindDataSource(clz, dataSource, DBType.MYSQL);
         int step = 10;
         int startindex = 0;
         while (true) {
             Table t = clz.getDeclaredConstructor().newInstance();
             t.limit(startindex, step);
-            List<T> list = t.selects(); //数据查询
+            List<T> list = t.selects(); //query data
             if (list != null)
-                saveByClass(clz, list);
-                System.out.println("save size:" + list.size());
+                saveByClass(toDB, clz, list);
+            System.out.println("save size:" + list.size());
             if (list == null || list.size() < step)
                 break;
             startindex = startindex + step;
@@ -51,13 +46,27 @@ public class DataTransfer {
 
     @Test
     public void TestTransferHstest1() throws JdaoException, JdaoClassException, SQLException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException {
-        transferByClass(Hstest1.class);
-        System.out.println("--------------完成表Hstest1数据从mysql移动到postgreSql---------------");
-        DataSource dataSource = DataSourceFactory.getDataSourceByPostgre();
-        Hstest1 hstest1 = new Hstest1().useDataSource(dataSource, DBType.POSTGRESQL);
-        List<Hstest1> list = hstest1.selects();
-        for (Hstest1 h : list) {
-            System.out.println(h);
+        DBhandle toDB = Jdao.newDBhandle(DataSourceFactory.getDataSourceByPostgre(), DBType.POSTGRESQL);
+        deleteTableData(toDB,Hstest1.class);
+        showTableData(toDB,Hstest1.class);
+        transferByClass(toDB, Hstest1.class);
+        System.out.println("--------------Complete Table Hstest1 data move from mysql to postgreSql---------------");
+        showTableData(toDB, Hstest1.class);
+    }
+
+   //delete from table
+    public <T extends Table<?>> void deleteTableData(DBhandle dBhandle, Class<T> clz) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, JdaoException, SQLException {
+        Table t = clz.getDeclaredConstructor().newInstance().useDBhandle(dBhandle);
+        int i = t.delete();
+        System.out.println("delete from table:" + t.TABLENAME() + ",delete size:" + i);
+    }
+
+    //show all data from table
+    public <T extends Table<?>> void showTableData(DBhandle dBhandle, Class<T> clz) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, JdaoException, JdaoClassException, SQLException {
+        Table t = clz.getDeclaredConstructor().newInstance().useDBhandle(dBhandle);
+        List<T> list = t.selects();
+        for (T v : list) {
+            System.out.println(v);
         }
     }
 
