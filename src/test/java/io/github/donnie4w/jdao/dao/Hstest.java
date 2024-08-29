93d74337e696645223a3758b573905e7fdabf119
donnie4w <donnie4w@gmail.com>
2024-08-29 16:58:31 +0800

    Update Hstest.java

--- src/test/java/io/github/donnie4w/jdao/dao/Hstest.java
+++ src/test/java/io/github/donnie4w/jdao/dao/Hstest.java
@@ -1,239 +1,272 @@
 /*
- * Copyright (c) 2024, donnie4w <donnie4w@gmail.com> All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * github.com/donnie4w/jdao
- */
+* Copyright (c) 2024, donnie4w <donnie4w@gmail.com> All rights reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+* github.com/donnie4w/jdao
+*/
 
 package io.github.donnie4w.jdao.dao;
 
-import io.github.donnie4w.jdao.base.Fields;
-import io.github.donnie4w.jdao.base.JStruct;
-import io.github.donnie4w.jdao.base.Table;
-import io.github.donnie4w.jdao.base.Util;
-import io.github.donnie4w.jdao.handle.JdaoException;
-import io.github.donnie4w.jdao.util.Serializer;
+import java.util.Objects;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Arrays;
-import java.util.Objects;
+import io.github.donnie4w.jdao.base.Fields;
+import io.github.donnie4w.jdao.base.Table;
+import io.github.donnie4w.jdao.base.Util;
+import io.github.donnie4w.jdao.util.Serializer;
+import io.github.donnie4w.jdao.handle.JdaoException;
 import java.util.Date;
-
+import java.util.Arrays;
 /**
- * dbtype:sqlite ,database:hstest.db ,table:hstest
+ * dbtype: mysql, database: , table: hstest
  *
  * @version jdao version 2.0.1
- * @date 2024-07-24 14:37:41 
+ * @date 2024-08-23 19:14:24
  */
 public class Hstest extends Table<Hstest> {
 
-	private static final long serialVersionUID = 6118074828633154000L;
+    private static final long serialVersionUID = 6118074828633154000L;
 
-	private final static String TABLENAME_ = "hstest";
+    private final static String TABLENAME_ = "hstest";
+    public final static Fields<Hstest> ID = new Fields("id");
+    public final static Fields<Hstest> AGE = new Fields("age");
+    public final static Fields<Hstest> ROWNAME = new Fields("rowname");
+    public final static Fields<Hstest> VALUE = new Fields("value");
+    public final static Fields<Hstest> UPDATETIME = new Fields("updatetime");
+    public final static Fields<Hstest> BODY = new Fields("body");
+    public final static Fields<Hstest> FLOA = new Fields("floa");
+    public final static Fields<Hstest> LEVEL = new Fields("level");
+    public final static Fields<Hstest> CREATED_AT = new Fields("created_at");
+    public final static Fields<Hstest> UPDATED_AT = new Fields("updated_at");
+    public final static Fields<Hstest> DELETED_AT = new Fields("deleted_at");
 
-	public final static Fields<Hstest> ID = new Fields("id");
-	public final static Fields<Hstest> AGE = new Fields("age");
-	public final static Fields<Hstest> ROWNAME = new Fields("rowname");
-	public final static Fields<Hstest> VALUE = new Fields("value");
-	public final static Fields<Hstest> UPDATETIME = new Fields("updatetime");
-	public final static Fields<Hstest> BODY = new Fields("body");
-	public final static Fields<Hstest> FLOA = new Fields("floa");
-	public final static Fields<Hstest> LEVEL = new Fields("level");
-	
-	public Hstest() {
-		super(TABLENAME_, Hstest.class);
-		super.initFields(ID,AGE,ROWNAME,VALUE,UPDATETIME,BODY,FLOA,LEVEL);
-	}
+    public Hstest() {
+        super(TABLENAME_, Hstest.class);
+        super.initFields(ID, AGE, ROWNAME, VALUE, UPDATETIME, BODY, FLOA, LEVEL, CREATED_AT, UPDATED_AT, DELETED_AT);
+    }
 
-	public Hstest(String tableName) {
-		super(tableName, Hstest.class);
-		super.initFields(ID,AGE,ROWNAME,VALUE,UPDATETIME,BODY,FLOA,LEVEL);
-	}
+    public Hstest(String tableName) {
+        super(tableName, Hstest.class);
+        super.initFields(ID, AGE, ROWNAME, VALUE, UPDATETIME, BODY, FLOA, LEVEL, CREATED_AT, UPDATED_AT, DELETED_AT);
+    }
+    private int id;
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        fieldPut(ID, id);
+        this.id = id;
+    }
 
-	@Override
-	public void toJdao() {
-		super.init(Hstest.class);
-	}
+    private long age;
+    public long getAge() {
+        return this.age;
+    }
+    public void setAge(long age) {
+        fieldPut(AGE, age);
+        this.age = age;
+    }
 
-	private long id;
-	private long age;
-	private String rowname;
-	private String value;
-	private Date updatetime;
-	private byte[] body;
-	private double floa;
-	private long level;
+    private String rowname;
+    public String getRowname() {
+        return this.rowname;
+    }
+    public void setRowname(String rowname) {
+        fieldPut(ROWNAME, rowname);
+        this.rowname = rowname;
+    }
 
-	public long getId() {
-		return this.id;
-	}
+    private String value;
+    public String getValue() {
+        return this.value;
+    }
+    public void setValue(String value) {
+        fieldPut(VALUE, value);
+        this.value = value;
+    }
 
-	public void setId(long id) {
-		fieldPut(ID, id);
-		this.id = id;
-	}
+    private Date updatetime;
+    public Date getUpdatetime() {
+        return this.updatetime;
+    }
+    public void setUpdatetime(Date updatetime) {
+        fieldPut(UPDATETIME, updatetime);
+        this.updatetime = updatetime;
+    }
 
-	public long getAge() {
-		return this.age;
-	}
+    private byte[] body;
+    public byte[] getBody() {
+        return this.body;
+    }
+    public void setBody(byte[] body) {
+        fieldPut(BODY, body);
+        this.body = body;
+    }
 
-	public void setAge(long age) {
-		fieldPut(AGE, age);
-		this.age = age;
-	}
+    private byte[] floa;
+    public byte[] getFloa() {
+        return this.floa;
+    }
+    public void setFloa(byte[] floa) {
+        fieldPut(FLOA, floa);
+        this.floa = floa;
+    }
 
-	public String getRowname() {
-		return this.rowname;
-	}
+    private long level;
+    public long getLevel() {
+        return this.level;
+    }
+    public void setLevel(long level) {
+        fieldPut(LEVEL, level);
+        this.level = level;
+    }
 
-	public void setRowname(String rowname) {
-		fieldPut(ROWNAME, rowname);
-		this.rowname = rowname;
-	}
+    private Date created_at;
+    public Date getCreated_at() {
+        return this.created_at;
+    }
+    public void setCreated_at(Date created_at) {
+        fieldPut(CREATED_AT, created_at);
+        this.created_at = created_at;
+    }
 
-	public String getValue() {
-		return this.value;
-	}
+    private Date updated_at;
+    public Date getUpdated_at() {
+        return this.updated_at;
+    }
+    public void setUpdated_at(Date updated_at) {
+        fieldPut(UPDATED_AT, updated_at);
+        this.updated_at = updated_at;
+    }
 
-	public void setValue(String value) {
-		fieldPut(VALUE, value);
-		this.value = value;
-	}
+    private Date deleted_at;
+    public Date getDeleted_at() {
+        return this.deleted_at;
+    }
+    public void setDeleted_at(Date deleted_at) {
+        fieldPut(DELETED_AT, deleted_at);
+        this.deleted_at = deleted_at;
+    }
 
-	public Date getUpdatetime() {
-		return this.updatetime;
-	}
 
-	public void setUpdatetime(Date updatetime) {
-		fieldPut(UPDATETIME, updatetime);
-		this.updatetime = updatetime;
-	}
+    @Override
+    public String toString() {
+        return "id:" + id + ", " + "age:" + age + ", " + "rowname:" + rowname + ", " + "value:" + value + ", " + "updatetime:" + updatetime + ", " + "body:" + body + ", " + "floa:" + floa + ", " + "level:" + level + ", " + "created_at:" + created_at + ", " + "updated_at:" + updated_at + ", " + "deleted_at:" + deleted_at;
+    }
 
-	public byte[] getBody() {
-		return this.body;
-	}
+    @Override
+    public Hstest copy(Hstest h) {
+        this.setId(h.getId());
+        this.setAge(h.getAge());
+        this.setRowname(h.getRowname());
+        this.setValue(h.getValue());
+        this.setUpdatetime(h.getUpdatetime());
+        this.setBody(h.getBody());
+        this.setFloa(h.getFloa());
+        this.setLevel(h.getLevel());
+        this.setCreated_at(h.getCreated_at());
+        this.setUpdated_at(h.getUpdated_at());
+        this.setDeleted_at(h.getDeleted_at());
+        return this;
+    }
 
-	public void setBody(byte[] body) {
-		fieldPut(BODY, body);
-		this.body = body;
-	}
+    @Override
+    public void scan(String fieldname, Object obj) throws JdaoException {
+        try {
+            switch (fieldname) {
+                case "id":
+                    setId(Util.asInt(obj));
+                    break;
+                case "age":
+                    setAge(Util.asLong(obj));
+                    break;
+                case "rowname":
+                    setRowname(Util.asString(obj));
+                    break;
+                case "value":
+                    setValue(Util.asString(obj));
+                    break;
+                case "updatetime":
+                    setUpdatetime(Util.asDate(obj));
+                    break;
+                case "body":
+                    setBody(Util.asBytes(obj));
+                    break;
+                case "floa":
+                    setFloa(Util.asBytes(obj));
+                    break;
+                case "level":
+                    setLevel(Util.asLong(obj));
+                    break;
+                case "created_at":
+                    setCreated_at(Util.asDate(obj));
+                    break;
+                case "updated_at":
+                    setUpdated_at(Util.asDate(obj));
+                    break;
+                case "deleted_at":
+                    setDeleted_at(Util.asDate(obj));
+                    break;
+            }
+        } catch (Exception e) {
+            throw new JdaoException(e);
+        }
+    }
 
-	public double getFloa() {
-		return this.floa;
-	}
-
-	public void setFloa(double floa) {
-		fieldPut(FLOA, floa);
-		this.floa = floa;
-	}
-
-	public long getLevel() {
-		return this.level;
-	}
-
-	public void setLevel(long level) {
-		fieldPut(LEVEL, level);
-		this.level = level;
-	}
-
-	@Override
-	public String toString() {
-		return "id:" + id + " , " + "age:" + age + " , " + "rowname:" + rowname + " , " + "value:" + value + " , " + "updatetime:" + updatetime + " , " + "body:" + body + " , " + "floa:" + floa + " , " + "level:" + level;
-	}
-
-	@Override
-	public Hstest copy(Hstest h) {
-		this.setId(h.getId());
-		this.setAge(h.getAge());
-		this.setRowname(h.getRowname());
-		this.setValue(h.getValue());
-		this.setUpdatetime(h.getUpdatetime());
-		this.setBody(h.getBody());
-		this.setFloa(h.getFloa());
-		this.setLevel(h.getLevel());
-		return this;
-	}
-
-	@Override
-	public void scan(String fieldname, Object obj) throws JdaoException {
-		try {
-			switch (fieldname) {
-				case "id":
-					setId(Util.asLong(obj));
- 					break;
-				case "age":
-					setAge(Util.asLong(obj));
- 					break;
-				case "rowname":
-					setRowname(Util.asString(obj));
- 					break;
-				case "value":
-					setValue(Util.asString(obj));
- 					break;
-				case "updatetime":
-					setUpdatetime(Util.asDate(obj));
- 					break;
-				case "body":
-					setBody(Util.asBytes(obj));
- 					break;
-				case "floa":
-					setFloa(Util.asDouble(obj));
- 					break;
-				case "level":
-					setLevel(Util.asLong(obj));
- 					break;
-			}
-		} catch (Exception e) {
-			throw new JdaoException(e);
-		}
-	}
-
-	@Override
-	public byte[] encode() {
-		Map<String, Object> map = new HashMap();
-		map.put("id", this.getId());
-		map.put("age", this.getAge());
-		map.put("rowname", this.getRowname());
-		map.put("value", this.getValue());
-		map.put("updatetime", this.getUpdatetime());
-		map.put("body", this.getBody());
-		map.put("floa", this.getFloa());
-		map.put("level", this.getLevel());
-		return Serializer.encode(map);
-	}
-
-	@Override
-	public Hstest decode(byte[] bs) throws JdaoException {
-		Map<String, Object> map = Serializer.decode(bs);
-		if (map != null) {
-			for (Map.Entry<String, Object> entry : map.entrySet()) {
-				scan(entry.getKey(), entry.getValue());
-			}
-		}
-		return this;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Hstest _Hstest = (Hstest) o;
-		return id == _Hstest.id && age == _Hstest.age && Objects.equals(rowname, _Hstest.rowname) && Objects.equals(value, _Hstest.value) && Objects.equals(updatetime, _Hstest.updatetime) && Objects.deepEquals(body, _Hstest.body) && floa == _Hstest.floa && level == _Hstest.level;
-	}
+        return id == _Hstest.id && age == _Hstest.age && Objects.equals(rowname, _Hstest.rowname) && Objects.equals(value, _Hstest.value) && Objects.equals(updatetime, _Hstest.updatetime) && Objects.deepEquals(body, _Hstest.body) && Objects.deepEquals(floa, _Hstest.floa) && level == _Hstest.level && Objects.equals(created_at, _Hstest.created_at) && Objects.equals(updated_at, _Hstest.updated_at) && Objects.equals(deleted_at, _Hstest.deleted_at);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id , age , rowname , value , updatetime , Arrays.hashCode(body) , floa , level);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, age, rowname, value, updatetime, Arrays.hashCode(body), Arrays.hashCode(floa), level, created_at, updated_at, deleted_at);
+    }
+
+    @Override
+    public byte[] encode() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", this.getId());
+        map.put("age", this.getAge());
+        map.put("rowname", this.getRowname());
+        map.put("value", this.getValue());
+        map.put("updatetime", this.getUpdatetime());
+        map.put("body", this.getBody());
+        map.put("floa", this.getFloa());
+        map.put("level", this.getLevel());
+        map.put("created_at", this.getCreated_at());
+        map.put("updated_at", this.getUpdated_at());
+        map.put("deleted_at", this.getDeleted_at());
+        return Serializer.encode(map);
+    }
+
+    @Override
+    public Hstest decode(byte[] bs) throws JdaoException {
+        Map<String, Object> map = Serializer.decode(bs);
+        if (map != null) {
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                scan(entry.getKey(), entry.getValue());
+            }
+        }
+        return this;
+    }
+
+    @Override
+    public void toJdao() {
+        super.init(Hstest.class);
+    }
 }
